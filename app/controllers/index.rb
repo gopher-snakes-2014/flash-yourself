bc3d2c3805d856ff21a211a25b96d68b7f7d7483
shakrah yves & Lady Danger <pair+syves.yenhtran@devbootcamp.com>
2014-09-18 16:28:44 -0700

    hope this works

---
+++ app/controllers/index.rb
@@ -0,0 +1,33 @@
+#displays the decks
+get '/' do
+  @decks = Deck.all
+  @cards = Card.all
+  erb :index
+end
+
+#takes you to deck by id ex 'fruits',
+#shows all the cards for a specific deck
+
+get '/deck/:id' do
+
+   session[:current_deck] = Deck.find( params[:id] ) unless params[:id].nil?
+
+   session[:cards_left_in_deck] = session[:current_deck].cards.clone
+   session[:current_card] = session[:cards_left_in_deck].pop
+
+   erb :show_deck
+end
+
+post "/answer" do
+  answer = params[:user_answer]
+
+  if answer == session[:current_card].answer
+    session[:current_card_answered?] = true
+  else
+    session[:current_card_answered?] = false
+  end
+
+  session[:current_card] = session[:cards_left_in_deck].pop if session[:current_card_answered?]
+
+  redirect '/deck'
+end
