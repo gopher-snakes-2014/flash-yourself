f795c8d4dc81b7b7a6d0ca830176dc15f5c09cf9
Michael T-Rexman <michael.a.trestman@gmail.com>
2014-09-19 11:15:29 -0700

    does some cool bizness

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -5,6 +5,29 @@ get '/' do
   erb :index
 end
 
+get '/new' do
+  erb :new_deck
+end
+
+post '/new' do
+  category = Deck.create(category: params[:category])
+  question1 = Card.new(question: params[:question_1], answer: params[:answer_1])
+  question1.deck = category
+  question1.save
+  question2 = Card.new(question: params[:question_2], answer: params[:answer_2])
+  question2.deck = category
+  question2.save
+  question3 = Card.new(question: params[:question_3], answer: params[:answer_3])
+  question3.deck = category
+  question3.save
+  question4 = Card.new(question: params[:question_4], answer: params[:answer_4])
+  question4.deck = category
+  question4.save
+  redirect '/'
+
+end
+
+
 #takes you to deck by id ex 'fruits',
 #shows all the cards for a specific deck
 
